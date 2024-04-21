9c1b7bcb95c0e7d5bda879bf94646b326e23772c
Gintamier <gintamier@gmail.com>
2024-04-21 16:48:03 +0000

    .

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/start_screen.dart';
+import 'package:quizapp/quiz.dart';
+
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.blue, 
-                Colors.red, 
-                Colors.blue
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
